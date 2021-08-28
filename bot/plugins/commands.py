d911fdda542aef8319c11c22830ab49eba438d01
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-08-28 14:19:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -54,7 +54,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = " <b>Join <a href="https://t.me/worldmoviesaj">MOVIE HUB HD⬛️◼️◾️▪️</a> </b> \n <code>{file_name}</code> \n <b> <a href="https://t.me/AJmovieLINKS ">𝘼𝙇𝙇 𝙈𝙊𝙑𝙄𝙀𝙎 𝘼𝙉𝘿 𝙎𝙀𝙍𝙄𝙀𝙎 𝙃𝘿</a></b> \n <b>© Powered by <a href="https://t.me/AJmovieLINKS ">𝚈𝙾𝚄 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝚆𝙴 𝙿𝚁𝙾𝚅𝙸𝙳𝙴</a></b>",
+                caption = f" <code> {file_name} </code>\n@AJmovieLINKS\n@worldmoviesaj",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
