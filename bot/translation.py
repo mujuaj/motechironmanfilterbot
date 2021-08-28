bb3942d325f6269cf12b48c4074baa640762d941
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-08-28 11:14:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,13 +6,13 @@ class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
 
-<b>I am a Pro AutoFilterBot V2....😜</b>
+<b>I am a Pro AJ AutoFilterBot V2....😜</b>
 
-<b>Make me an admin for your group and channel then connect me....🎉</b>
+<b>I am exclusively made for @worldmoviesaj ....🎉</b>
 
-<b>🙂Watch the video below to build an Pro Autofilter bot V2 like me...</b>
+<b>Join us for more movies...</b>
 
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
+<b>Subscribe to  @worldmoviesaj...</b>
 
 <b>Press /help to know about available commands🤪</b>"""    
     
@@ -46,18 +46,18 @@ class Translation(object):
             
             -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
             
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/avataradorn">© AJ </a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro AJ Auto Filter Bot</b>
 
-<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
+<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/avataradorn">�AJ</a></b>
 
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
+<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/worldmoviesaj">👤 ITS YOU</a></b>
 
 <b>➥ 🗣️Language</b> : <b>Python3<b>
 
-<b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
+<b>➥ 📚Library</b> : <b><a href="https://t.me/worldmoviesaj">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
+<b>➥ 📖Source Code</b> : <b><a href="https://t.me/AJmovieLINKS">💥Click Me</a></b>
 """
